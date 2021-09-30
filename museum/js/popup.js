494489f4fc9f06cd94da9dd42ad165aecfd6648f
Olga Tolpykina <olga.belevantseva@gmail.com>
2021-09-30 20:04:34 +0300

    feat: add overlay to popup window

--- museum/js/popup.js
+++ museum/js/popup.js
@@ -2,7 +2,24 @@
 
 let buyButton = document.querySelector('.tickets__buy');
 let closeButton = document.querySelector('.button__close');
+let overlay = document.querySelector('.overlay');
 let popupWindow = document.querySelector('.buy');
 
-buyButton.addEventListener('mousedown', () => popupWindow.classList.add('buy_popup'));
-closeButton.addEventListener('mousedown', () => popupWindow.classList.remove('buy_popup'));
+buyButton.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    popupWindow.classList.add('buy_popup');
+    overlay.classList.add('overlay_popup');
+});
+
+
+closeButton.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    popupWindow.classList.remove('buy_popup');
+    overlay.classList.remove('overlay_popup');
+});
+
+overlay.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    popupWindow.classList.remove('buy_popup');
+    overlay.classList.remove('overlay_popup');
+});
