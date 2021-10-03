85ab4e09b2c2f7c7f445f53cc1c77fea43e07e1d
Olga Tolpykina <olga.belevantseva@gmail.com>
2021-10-04 01:42:32 +0300

    feat: add styles for tablet

--- museum-adaptive/js/burger.js
+++ museum-adaptive/js/burger.js
@@ -6,12 +6,23 @@ let welcomeContent = document.querySelector('.welcome__content');
 let navigationMobileLink = document.querySelector('.navigation_mobile__link');
 let main = document.querySelector('main');
 let footer = document.querySelector('footer');
+let body = document.querySelector('body');
+let footerLogo = document.querySelector('.footer__logo');
+let footerNavigation = document.querySelector('.footer-navigation');
+let footerBottomLine = document.querySelector('.footer__bottomline');
+let footerSocials = document.querySelector('.footer-nav__socials');
 
 burger.addEventListener('mousedown', (event) => {
     event.preventDefault();
     burger.classList.toggle('burger-popup');
     navigationMobile.classList.toggle('navigation_mobile-active');
     welcomeContent.classList.toggle('welcome__content-hidden');
+    main.classList.toggle('hidden');
+    body.classList.toggle('active-menu');
+    footerLogo.classList.toggle('hidden');
+    footerNavigation.classList.toggle('hidden');
+    footerBottomLine.classList.toggle('hidden');
+    footerSocials.classList.toggle('active-menu');
 });
 
 navigationMobile.addEventListener('mousedown', (event) => {
@@ -19,6 +30,12 @@ navigationMobile.addEventListener('mousedown', (event) => {
     burger.classList.toggle('burger-popup');
     navigationMobile.classList.toggle('navigation_mobile-active');
     welcomeContent.classList.toggle('welcome__content-hidden');
+    main.classList.toggle('hidden');
+    body.classList.toggle('active-menu');
+    footerLogo.classList.toggle('hidden');
+    footerNavigation.classList.toggle('hidden');
+    footerBottomLine.classList.toggle('hidden');
+    footerSocials.classList.toggle('active-menu');
 });
 
 main.addEventListener('mousedown', (event) => {
@@ -26,6 +43,12 @@ main.addEventListener('mousedown', (event) => {
     burger.classList.toggle('burger-popup');
     navigationMobile.classList.toggle('navigation_mobile-active');
     welcomeContent.classList.toggle('welcome__content-hidden');
+    main.classList.toggle('hidden');
+    body.classList.toggle('active-menu');
+    footerLogo.classList.toggle('hidden');
+    footerNavigation.classList.toggle('hidden');
+    footerBottomLine.classList.toggle('hidden');
+    footerSocials.classList.toggle('active-menu');
 });
 
 footer.addEventListener('mousedown', (event) => {
@@ -33,4 +56,10 @@ footer.addEventListener('mousedown', (event) => {
     burger.classList.toggle('burger-popup');
     navigationMobile.classList.toggle('navigation_mobile-active');
     welcomeContent.classList.toggle('welcome__content-hidden');
+    main.classList.toggle('hidden');
+    body.classList.toggle('active-menu');
+    footerLogo.classList.toggle('hidden');
+    footerNavigation.classList.toggle('hidden');
+    footerBottomLine.classList.toggle('hidden');
+    footerSocials.classList.toggle('active-menu');
 });
