bbaf139567cfadc783583dd3752665f3ccad8417
Olga Tolpykina <olga.belevantseva@gmail.com>
2021-10-08 15:10:08 +0300

    feat: change folder name to museum-dom

---
+++ museum-dom/js/burger.js
@@ -0,0 +1,69 @@
+'use strict';
+
+let burger = document.querySelector('.burger');
+let navigationMobile = document.querySelector('.navigation_mobile');
+let welcomeContent = document.querySelector('.welcome__content');
+let navigationMobileLink = document.querySelector('.navigation_mobile__link');
+let main = document.querySelector('main');
+let footer = document.querySelector('footer');
+let body = document.querySelector('body');
+let footerLogo = document.querySelector('.footer__logo');
+let footerNavigation = document.querySelector('.footer-navigation');
+let footerBottomLine = document.querySelector('.footer__bottomline');
+let footerSocials = document.querySelector('.footer-nav__socials');
+let hr = document.querySelector('.line');
+
+burger.addEventListener('mousedown', (event) => {
+    event.preventDefault();
+    burger.classList.toggle('burger-popup');
+    navigationMobile.classList.toggle('navigation_mobile-active');
+    welcomeContent.classList.toggle('welcome__content-hidden');
+    main.classList.toggle('hidden');
+    body.classList.toggle('active-menu');
+    footerLogo.classList.toggle('hidden');
+    footerNavigation.classList.toggle('hidden');
+    footerBottomLine.classList.toggle('hidden');
+    footerSocials.classList.toggle('active-menu');
+    hr.classList.toggle('hidden');
+});
+
+navigationMobile.addEventListener('mousedown', (event) => {
+    event.preventDefault();
+    burger.classList.toggle('burger-popup');
+    navigationMobile.classList.toggle('navigation_mobile-active');
+    welcomeContent.classList.toggle('welcome__content-hidden');
+    main.classList.toggle('hidden');
+    body.classList.toggle('active-menu');
+    footerLogo.classList.toggle('hidden');
+    footerNavigation.classList.toggle('hidden');
+    footerBottomLine.classList.toggle('hidden');
+    footerSocials.classList.toggle('active-menu');
+    hr.classList.toggle('hidden');
+});
+
+// main.addEventListener('mousedown', (event) => {
+//     event.preventDefault();
+//     burger.classList.toggle('burger-popup');
+//     navigationMobile.classList.toggle('navigation_mobile-active');
+//     welcomeContent.classList.toggle('welcome__content-hidden');
+//     main.classList.toggle('hidden');
+//     body.classList.toggle('active-menu');
+//     footerLogo.classList.toggle('hidden');
+//     footerNavigation.classList.toggle('hidden');
+//     footerBottomLine.classList.toggle('hidden');
+//     footerSocials.classList.toggle('active-menu');
+// });
+
+footer.addEventListener('mousedown', (event) => {
+    event.preventDefault();
+    burger.classList.toggle('burger-popup');
+    navigationMobile.classList.toggle('navigation_mobile-active');
+    welcomeContent.classList.toggle('welcome__content-hidden');
+    main.classList.toggle('hidden');
+    body.classList.toggle('active-menu');
+    footerLogo.classList.toggle('hidden');
+    footerNavigation.classList.toggle('hidden');
+    footerBottomLine.classList.toggle('hidden');
+    footerSocials.classList.toggle('active-menu');
+    hr.classList.toggle('hidden');
+});
